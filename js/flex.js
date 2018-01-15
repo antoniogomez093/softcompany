6dd9db170e2ec556b592ce7de7eba888d8b55476
antoniogomez093 <antoniogomez093@gmail.com>
2018-01-15 12:49:59 -0600

    Pagina al 90%

---
+++ js/flex.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+
+	   $('#testimonial-slider').flexslider({
+	   	namespace: "flex-",
+	      controlsContainer: "",
+	      animation: 'slide',
+	      controlNav: true,
+	      directionNav: true,
+	      smoothHeight: true,
+	      slideshowSpeed: 10000,
+	      animationSpeed: 600,
+	      randomize: false,
+	      touch: true,
+	   });
+
+   });
