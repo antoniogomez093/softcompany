ad5040a536bfd60626d413522823359a8de0abe7
antoniogomez093 <antoniogomez093@gmail.com>
2018-01-25 10:47:11 -0600

    Correo de softcompany

--- formulario.php
+++ formulario.php
@@ -13,13 +13,13 @@ $header .= "Mine-Version: 1.0 \r\n";
 $header .="Content-Type: text/plain";
 
 $mensaje= "Este mensaje fue enviado por ".$nombre.$apellido.",\r\n";
-$mensaje.="Su celular es: ". $celular ."\r\n";
-$mensaje.="Su telefono es: ". $telefono ."\r\n";
-$mensaje.="Su email es: ". $email ."\r\n";
-$mensaje.="Su aplicacion es". $tipodeaplicacion."\r\n";
+$mensaje.="Celular: ". $celular ."\r\n";
+$mensaje.="Teléfono: ". $telefono ."\r\n";
+$mensaje.="E-mail: ". $email ."\r\n";
+$mensaje.="Tipo de aplicación". $tipodeaplicacion."\r\n";
 $mensaje.="Mensaje: ".$_POST['mensaje']." \r\n";
 $mensaje.="Enviado el ".date('d/m/Y',time());
-$para='antoniogomez093@gmail.com';
+$para='sc@softcompany.com.gt';
 $asunto='Asunto del e-mail recibido';
 mail($para,$asunto,utf8_decode($mensaje),$header);
 header("Location:index.html")
